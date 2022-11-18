4c3cace17368d3884c4521381ee946017b94b578
¨agus¨ <aguspizarro49@gmail.com>
2022-11-17 23:52:52 -0300

    primer desafio

---
+++ app.js
@@ -0,0 +1,29 @@
+class Usuario {
+    constructor(nombre, apellido, libros, mascotas){
+        this.nombre = "Gonzalo";
+        this.apellido = "Gonzales";
+        this.libros = ["Libro 1", "Libro 2", "Libro 3"];
+        this.mascotas ("Perro", "Gato");
+    }
+
+    static totalMascotas = 2
+
+    getFullName() {
+        return fuction (
+            this.nombre,
+            this.apellido,
+        ) 
+    }
+
+    addMascota
+
+
+    countMascotas() {
+        return Usuario.totalMascotas;
+    }
+
+    getBookNames() {
+        return this.libros;
+    }
+}
+
